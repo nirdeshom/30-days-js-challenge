1f1b32e003d9a622f558ace868153d28e320a095
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-07-26 16:36:45 +0530

    Activity 3: Comparison Operation

---
+++ day2/03_activity.js
@@ -0,0 +1,25 @@
+// Activity 3: Comparison Operation 
+
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+let larg = 5;
+let small = 4;
+console.log(larg > small);
+console.log(larg < small);
+
+
+//  Task 9: Write a Program to compare two number using >= and <= and log the result to the console 
+let largNum = 6;
+let smallNum = 7;
+
+console.log(largNum >= smallNum);
+console.log(largNum <= smallNum);
+
+
+// Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+
+console.log(33 == 33);
+console.log(33 == "33");
+
+console.log(33 == 33);
+console.log(33 == "33");
+
