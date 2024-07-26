0e7426849f1420e502fabb2d675e768c2ad158ec
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-07-26 17:09:03 +0530

    Activity 5: Ternary Operators

---
+++ day2/05_activity.js
@@ -0,0 +1,8 @@
+// Activity 5: Ternary Operator
+
+// Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+let score = -4;
+
+let results = (score >= 0) ? "positive" : "negative";
+console.log(results);
