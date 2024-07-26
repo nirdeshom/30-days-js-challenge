04265f93e512179b921d8d06ff65136ccce349a0
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-07-26 16:35:45 +0530

    Activity 2: Assignment Operation

---
+++ day2/02_activity.js
@@ -0,0 +1,10 @@
+// Activity 2: Assignment Operation 
+// Task 6: Use the += operator to add a number to a variable and log the result to the console.
+let score = 99;
+score += 1
+console.log(score);
+
+// Task 7: Use the -= operator to subtract a number from a variable and log the result to the console
+let myScore = 100;
+myScore -= 1
+console.log(myScore);
