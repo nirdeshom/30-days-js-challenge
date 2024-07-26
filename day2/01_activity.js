414fd1a18635fd8c9aab33a87a4a937c90aaf501
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-07-26 16:34:33 +0530

    Activity 1: Arithmetic Operatons

---
+++ day2/01_activity.js
@@ -0,0 +1,29 @@
+//  Activity 1: Arithmetic Operations
+
+// Taks 1: Write a program to add two numbers and log the result to the console.
+console.log(2 + 4);  // output: 6
+
+
+// Task 2: Write a program to subtract two numbers and log the result to the console.
+console.log(4 - 3);  // output: 1
+
+
+// Task 3: Write a program to multiply two numbers and log the result to the console.
+let a = 3; b = 4;
+console.log(a * b);  // output: 12
+
+
+// Task 4: Write a program to divide two numbers and log the result to the console.
+let num1 = 5;
+let num2 = 2;
+let sum = num1 / num2;
+console.log(sum);  // output: 2.5
+
+
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console
+const x = 9;
+const y = 2;
+const reminder = x % y;
+const divided = reminder / 2;
+console.log(reminder);  // output: 1
+console.log(divided);  // output: 0.5
