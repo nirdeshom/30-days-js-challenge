df5a4a6cb64c65c61813e3f3071af81c532c8871
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-09-14 20:53:04 +0530

    Activity 1: If-Else Statements

---
+++ day3/01_activity.js
@@ -0,0 +1,19 @@
+// Activity 1: If-Else Statements
+
+// Task 1: Write a program to check if a number is positive, negative, or zero, and log the result to the console.
+let number = -10;
+if (number > 0) {
+    console.log(`Positive Number ${number}`)
+} else if(number < 0){
+    console.log(`Negative Number ${number}`);
+}else {
+    console.log(`The Zero number ${number}`);
+}
+
+// Task 2: Write a program to check if a person is eligible to vote (age >= 18) and log the result to the console.
+let age = 8;
+if (age >= 18) {
+    console.log(`A Person is Eligible to Vote age: ${age} year`);
+} else {
+    console.log(`A Person is Not Eligible to Vote age: ${age} year`);
+}
